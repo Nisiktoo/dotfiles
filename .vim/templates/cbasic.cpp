4eb2e0ef0432bec238e9f1f1e37519dc805f3295
Mahfuz Bin Minhaz <mahfuz@duck.com>
2023-06-29 22:09:49 +0600

    Create cbasic.cpp

---
+++ .vim/templates/cbasic.cpp
@@ -0,0 +1,85 @@
+/* 
+ * You didn’t come into this world. 
+ * You came out of it, like a wave from the ocean.
+ * You are not a stranger here. 
+				– Alan Watts
+									 */
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cstdint>
+#include <complex>
+#include <cmath>
+#include <chrono>
+#include <ctime>
+#include <cassert>
+#include <list>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <string>
+#include <random>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+using pll = pair<ll, ll>;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)(x).size()
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (a)-1; i >= b; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define nl putchar('\n')
+#define putc(c) putchar(c)
+#define ins insert
+#define sci(x) scanf("%d", &x)
+#define sci2(a,b) scanf("%d %d", &a, &b)
+#define sci3(a,b,c) scanf("%d %d %d", &a, &b, &c)
+#define sci4(a,b,c,d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define pri(x) printf("%d ", x)
+#define prin(x) printf("%d\n", x)
+#define scl(x) scanf("%lld", &x)
+#define scl2(a,b) scanf("%lld %lld", &a, &b)
+#define scl3(a,b,c) scanf("%lld %lld %lld", &a, &b, &c)
+#define prl(x) printf("%lld ", x)
+#define prln(x) printf("%lld\n", x)
+#define scs(s) scanf("%s", s)
+#define scs2(s1, s2) scanf("%s %s", s1, s2)
+#define RET return
+
+
+
+
+int main(void) {
+	/* freopen("input.txt", "r", stdin); */
+	/* freopen("output.txt", "w", stdout); */
+	 
+	RET 0;
+}
