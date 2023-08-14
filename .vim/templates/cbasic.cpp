8f9d55cf662679cbf40f445f3b42f6deec99858d
17ShaRat <mahfuz@duck.com>
2023-08-14 18:09:01 +0600

    Modified

--- .vim/templates/cbasic.cpp
+++ .vim/templates/cbasic.cpp
@@ -16,6 +16,7 @@
 #include <chrono>
 #include <ctime>
 #include <cassert>
+#include <iostream>
 #include <list>
 #include <vector>
 #include <set>
@@ -48,6 +49,37 @@ typedef vector<ll> vl;
 typedef vector<pii> vpii;
 typedef vector<pll> vpll;
 typedef vector<bool> vb;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x);
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef DEBUG
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#else
+#define dbg(x...)
+#endif
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 #define pb push_back
 #define mp make_pair
 #define all(x) (x).begin(), (x).end()
@@ -81,7 +113,6 @@ typedef vector<bool> vb;
 #define scs2(s1, s2) scanf("%s %s", s1, s2)
 #define RET return
 const int MOD = 1000000007;
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 
 int main(void) {
