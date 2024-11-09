64a7dfaf9b3b97408e2295b804728c01ae9a99aa
Mahfuz Bin Minhaz <mahfuz@duck.com>
2024-11-09 14:20:37 +0600

    cpp cp template

---
+++ cbasic1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+#ifdef DEBUG
+#include <debug.h>
+#else 
+#define dbg(x...)
+#endif
+
+void solve() {
+
+}
+
+int main() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        int _t = 1;
+        cin >> _t;
+        for (int i = 1; i <= _t; i++) {
+                solve();
+        }
+        return 0;
+}
