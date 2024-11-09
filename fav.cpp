f0b7ea4830aaba6ba5975ab84147c3e1ce173d81
Mahfuz Bin Minhaz <mahfuz@duck.com>
2024-11-09 14:21:11 +0600

    Add files via upload

---
+++ fav.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+#ifdef DEBUG
+#include <debug.h>
+#else 
+#define dbg(x...)
+#endif
+
+#define sci(x) scanf("%d", &x)
+#define scs(x) scanf("%s", x)
+#define sci2(x, y) scanf("%d %d", &x, &y)
+#define sci3(x, y, z) scanf("%d %d %d", &x, &y, &z)
+#define sci4(x, y, z, k) scanf("%d %d %d %d", &x, &y, &z, &k)
+#define scl(x) scanf("%lld", &x)
+#define scl2(x, y) scanf("%lld %lld", &x, &y)
+#define scl3(x, y, z) scanf("%lld %lld %lld", &x, &y, &z)
+#define scl4(x, y, z, k) scanf("%lld %lld %lld %lld", &x, &y, &z, &k)
+#define pri(x) printf("%d ", x)
+#define prl(x) printf("%lld ", x)
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+using pii = std::pair<int, int>;
+using pil = std::pair<int, ll>;
+using pll = std::pair<ll, ll>;
+using pli = std::pair<ll, int>;
+typedef std::vector<int> vi;
+typedef std::vector<ll> vl;
+typedef std::vector<pii> vpii;
+typedef std::vector<pil> vpil;
+typedef std::vector<pli> vpli;
+typedef std::vector<pll> vpll;
+typedef std::vector<bool> vb;
+
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int) (x).size()
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define F0Rd(i, a) for (int i = (a) - 1; i >= 0; i--)
+#define trav(a, x) for (auto& a : x)
+#define nl putchar('\n')
+#define ins insert
+#define RET return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void solve() {
+
+}
+
+int main() {
+        int _t = 1;
+        scanf("%d", &_t);
+        for (int i = 1; i <= _t; i++) {
+                solve();
+        }
+        return 0;
+}
